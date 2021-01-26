3c747a7bac5dc3f2e5a8d19fc73169b5c9fbaf5c
Philip Mateescu <github@philipm.at>
2021-01-26 10:46:55 -0600

    authentication required for claims transformation

--- Startup.cs
+++ Startup.cs
@@ -51,6 +51,7 @@ namespace AspNetCoreWindowsAuthClaims
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
