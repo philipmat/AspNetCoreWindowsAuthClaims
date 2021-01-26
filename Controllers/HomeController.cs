7084371a2414dd52841f32dd4e84f570744d01e1
Philip Mateescu <github@philipm.at>
2021-01-26 12:53:08 -0600

    minor usings cleanup

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
 using AspNetCoreWindowsAuthClaims.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreWindowsAuthClaims.Controllers
 {
