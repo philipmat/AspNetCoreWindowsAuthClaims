68fa14c95f55099276b454501bb0c99f6d6bed9c
Philip Mateescu <github@philipm.at>
2021-01-26 12:52:03 -0600

    conditionally uses an injected memory cache
    to make it cache under IIS Express, add "CacheClaims": "true" to the "IIS Express" profile in launchSettings.json

--- Auth/MagicPowersInfoProvider.cs
+++ Auth/MagicPowersInfoProvider.cs
@@ -1,12 +1,41 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetCoreWindowsAuthClaims.Auth
 {
     public class MagicPowersInfoProvider
     {
+        private const string CacheClaimsKey = "CacheClaims";
+        private const int ClaimCacheInSeconds = 5 * 60;
+        private readonly bool _cacheClaims;
+        private readonly IMemoryCache _memoryCache;
+
+        public MagicPowersInfoProvider(IConfiguration config, IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+            _cacheClaims = config.GetValue<bool>(CacheClaimsKey);
+        }
+
         public async Task<bool> CanHasPowerAsync(string userId)
         {
-            return await Task.FromResult(true);
+            if (!_cacheClaims)
+            {
+                return await ExpensiveHasPowerOperation(userId);
+            }
+
+            return await _memoryCache.GetOrCreateAsync<bool>(
+                $"power-{userId}",
+                async cacheEntry =>
+                {
+                    cacheEntry.SlidingExpiration = TimeSpan.FromSeconds(ClaimCacheInSeconds);
+                    bool hasPower = await ExpensiveHasPowerOperation(userId);
+                    return hasPower;
+                });
         }
+
+        private Task<bool> ExpensiveHasPowerOperation(string userId)
+            => Task.FromResult(true);
     }
 }
